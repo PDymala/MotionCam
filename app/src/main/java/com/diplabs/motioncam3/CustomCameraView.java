396bb0e6f95c992bf14de8c9e141f14c2515ac60
Piotr Dymala <p.dymala@gmail.com>
2021-12-18 14:01:55 +0100

    Fixed color filter (no impact on FPS), Changed to Camera2 API Added zoom on double tap Code is slightly a mess...

--- app/src/main/java/com/diplabs/motioncam3/CustomCameraView.java
+++ app/src/main/java/com/diplabs/motioncam3/CustomCameraView.java
@@ -1,6 +1,7 @@
 package com.diplabs.motioncam3;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.hardware.camera2.CameraAccessException;
@@ -9,9 +10,17 @@ import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.Size;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.math.MathUtils;
 
 import org.opencv.android.JavaCamera2View;
+import org.opencv.android.JavaCameraView;
 
 public class CustomCameraView extends JavaCamera2View {
     public CustomCameraView(Context context, int cameraId) {
@@ -20,24 +29,117 @@ public class CustomCameraView extends JavaCamera2View {
 
     public CustomCameraView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
     }
 
 
 
+    boolean firstTouch = false;
+    long time= System.currentTimeMillis();
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == event.ACTION_DOWN){
+            if(firstTouch && (System.currentTimeMillis() - time) <= 300) {
+                try {
+                    zoomUpCamera();
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
 
-public void zoomUp(){
+
+                Log.e("** DOUBLE TAP**"," second tap ");
+
+                firstTouch = false;
+
+            } else {
+                firstTouch = true;
+                time = System.currentTimeMillis();
+                Log.e("** SINGLE  TAP**"," First Tap time  "+time);
+                return false;
+            }
+        }
+        return true;
+    }
 
 
-}
+//
+//
+//
+    private int zoomCounter = 0;
+//    public void zoomUpCamera(){
+//
+//        Camera.Parameters params = mCamera.getParameters();
+//        zoomCounter =params.getMaxZoom()/5;
+//
+//        if (params.isZoomSupported()){
+//
+//            params.setZoom((params.getZoom() +zoomCounter)%params.getMaxZoom());
+//
+//            mCamera.setParameters(params);
+//
+//
+//        } else{
+//            //not supported
+//        }
+//
+//    }
+//thanks https://stackoverflow.com/questions/66016907/manipulate-javacamera2view-to-set-parameters-for-camera-device-opencv-in-andro
+    //to be read https://stackoverflow.com/questions/58902461/javacamera2view-pinch-zoom
+    // !! https://stackoverflow.com/questions/52568987/camera-zoom-setting-using-camera2-api
+    float zoomFloat;
+    float currentZoom = 1.0f;
+    public void zoomUpCamera() throws CameraAccessException {
 
-public Size[] getCameraSizes() throws CameraAccessException {
-    CameraManager manager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
-    CameraCharacteristics characteristics = manager.getCameraCharacteristics(manager.getCameraIdList()[0]);
-    StreamConfigurationMap streamConfigurationMap = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-    Size[] sizes = streamConfigurationMap.getOutputSizes(SurfaceTexture.class);
+        CameraManager mCameraManager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
 
-        return sizes;
-}
+        Zoom zoom = new Zoom( mCameraManager.getCameraCharacteristics(mCameraID));
+
+        zoomFloat = (zoom.maxZoom - Zoom.DEFAULT_ZOOM_FACTOR )/ 6;
+        currentZoom = (currentZoom + zoomFloat)%zoom.maxZoom;
+        zoom.setZoom(mPreviewRequestBuilder,currentZoom);
+
+        try {
+            mCaptureSession.setRepeatingRequest(mPreviewRequestBuilder.build(), null, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public double getCameraZoom(){
+//        Camera.Parameters params = mCamera.getParameters();
+//
+//        return ((double)params.getZoom() / (double)params.getMaxZoom());
+//    }
+
+//public Size[] getCameraSizes() throws CameraAccessException {
+//    CameraManager manager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
+//    CameraCharacteristics characteristics = manager.getCameraCharacteristics(manager.getCameraIdList()[0]);
+//    StreamConfigurationMap streamConfigurationMap = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+//    Size[] sizes = streamConfigurationMap.getOutputSizes(SurfaceTexture.class);
+//
+//        return sizes;
+//}
+
+    boolean flashlightOn = false;
+
+//    public void toggleFlashLight(){
+
+//        Camera.Parameters params = mCamera.getParameters();
+//        if (flashlightOn){
+//
+//            params.setFlashMode(params.FLASH_MODE_OFF);
+//            flashlightOn = false;
+//
+//        } else{
+//            params.setFlashMode(params.FLASH_MODE_TORCH);
+//            flashlightOn = true;
+//
+//
+//        }
+//        mCamera.setParameters(params);
+//
+
+//    }
 
 boolean enable = true;
     public void toggleFlashMode() throws CameraAccessException { //mPrevie changed from protected to public
@@ -57,6 +159,67 @@ boolean enable = true;
 
         } catch (CameraAccessException e) {
             e.printStackTrace();
+        }
+    }
+
+
+
+
+
+    public final class Zoom
+    {
+        private static final float DEFAULT_ZOOM_FACTOR = 1.0f;
+
+        @NonNull
+        private final Rect mCropRegion = new Rect();
+
+        public final float maxZoom;
+
+        @Nullable
+        private final Rect mSensorSize;
+
+        public final boolean hasSupport;
+
+        public Zoom(@NonNull final CameraCharacteristics characteristics)
+        {
+            this.mSensorSize = characteristics.get(CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE);
+
+            if (this.mSensorSize == null)
+            {
+                this.maxZoom = Zoom.DEFAULT_ZOOM_FACTOR;
+                this.hasSupport = false;
+                return;
+            }
+
+            final Float value = characteristics.get(CameraCharacteristics.SCALER_AVAILABLE_MAX_DIGITAL_ZOOM);
+
+            this.maxZoom = ((value == null) || (value < Zoom.DEFAULT_ZOOM_FACTOR))
+                    ? Zoom.DEFAULT_ZOOM_FACTOR
+                    : value;
+
+            this.hasSupport = (Float.compare(this.maxZoom, Zoom.DEFAULT_ZOOM_FACTOR) > 0);
+        }
+
+        public void setZoom(@NonNull final CaptureRequest.Builder builder, final float zoom)
+        {
+            if (this.hasSupport == false)
+            {
+                return;
+            }
+
+            final float newZoom = MathUtils.clamp(zoom, Zoom.DEFAULT_ZOOM_FACTOR, this.maxZoom);
+
+            final int centerX = this.mSensorSize.width() / 2;
+            final int centerY = this.mSensorSize.height() / 2;
+            final int deltaX  = (int)((0.5f * this.mSensorSize.width()) / newZoom);
+            final int deltaY  = (int)((0.5f * this.mSensorSize.height()) / newZoom);
+
+            this.mCropRegion.set(centerX - deltaX,
+                    centerY - deltaY,
+                    centerX + deltaX,
+                    centerY + deltaY);
+
+            builder.set(CaptureRequest.SCALER_CROP_REGION, this.mCropRegion);
         }
     }
 
--- app/src/main/java/com/diplabs/motioncam3/MainActivity.java
+++ app/src/main/java/com/diplabs/motioncam3/MainActivity.java
@@ -15,11 +15,13 @@ import android.util.Log;
 import android.util.Size;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -43,25 +45,39 @@ import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 import static android.content.ContentValues.TAG;
+import static org.opencv.core.Core.mixChannels;
+import static org.opencv.core.Core.split;
+import static org.opencv.core.CvType.CV_8UC1;
+import static org.opencv.core.CvType.CV_8UC3;
 
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2, PopupMenu.OnMenuItemClickListener{
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2, PopupMenu.OnMenuItemClickListener {
+
+    //TODO: Dodac menu do innej ilosci pikseli analizowanych
+    //TODO: DODAC w tym samym menu ilosc FRAME'ów do analizy (UWAGA, DO FPS TEZ!)
+    //TODO: CZY DA SIE ZMIENIC NAZWE?  taka juz istnieje...
+
+    // JAVA camera 2 dziala. jest zoom! :) ale cos fpsy nizsze niz przy camera 1 (?) Podobnot ak ejst....
+
 
     private static final String TAG = "FreqencyCam3";
     private static final int MY_CAMERA_REQUEST_CODE = 100;
-    private int activeCamera = CameraBridgeViewBase.CAMERA_ID_BACK;
+    private final int activeCamera = CameraBridgeViewBase.CAMERA_ID_BACK;
     private CustomCameraView javaCameraView;// JavaCameraView javaCameraView;
-    LineChart chart;
+    private LineChart chart;
     private BaseLoaderCallback baseLoaderCallback;
 
-    ImageButton imageButtonStopPlayCamera;
-    ImageButton imageButtonStopPlayGraph;
-    boolean cameraOn = true;
-    boolean graphOn = true;
+    private ImageButton imageButtonStopPlayCamera;
+    private ImageButton imageButtonStopPlayGraph;
+    private boolean cameraOn = true;
+    private boolean graphOn = true;
+    private int filterColorType = 3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +86,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         javaCameraView = (CustomCameraView) findViewById(R.id.cameraView1);//javaCameraView = (JavaCameraView) findViewById(R.id.cameraView1);
         chart = (LineChart) findViewById(R.id.chart);
+
 
         imageButtonStopPlayCamera = findViewById(R.id.imageButtonStopPlayCamView);
         imageButtonStopPlayGraph = findViewById(R.id.imageButtonStopPlayGraph);
@@ -83,7 +100,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             Log.d(TAG, "Permission prompt");
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
         }
-
 
 
         baseLoaderCallback = new BaseLoaderCallback(this) {
@@ -107,9 +123,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         };
 
 
-
-
     }
+
     static {
         System.loadLibrary("opencv_java4");
     }
@@ -129,36 +144,36 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         }
     }
 
-    public void stopPlayCam(View view){
+    public void stopPlayCam(View view) {
 
-        if (cameraOn){
+        if (cameraOn) {
             cameraOn = false;
             imageButtonStopPlayCamera.setImageResource(R.drawable.ic_baseline_play_circle_outline_24);
-        } else{
+        } else {
             cameraOn = true;
             imageButtonStopPlayCamera.setImageResource(R.drawable.ic_baseline_pause_circle_outline_24);
         }
 
     }
 
-    public void stopPlayGraph(View view){
-        if (graphOn){
+    public void stopPlayGraph(View view) {
+        if (graphOn) {
             graphOn = false;
             imageButtonStopPlayGraph.setImageResource(R.drawable.ic_baseline_play_circle_outline_24);
-        } else{
+        } else {
             graphOn = true;
             imageButtonStopPlayGraph.setImageResource(R.drawable.ic_baseline_pause_circle_outline_24);
         }
 
     }
-    public void showPopupSize(View view) {
+
+    public void showMenuColorFilter(View view) {
         PopupMenu popupMenu = new PopupMenu(this, view);
         popupMenu.setOnMenuItemClickListener(this);
         popupMenu.inflate(R.menu.mwnu);
         popupMenu.show();
     }
 
-    int filterColorType = 3;
 
     private void initializeCamera(CustomCameraView javaCameraView, int activeCamera) {
 
@@ -167,116 +182,176 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         javaCameraView.setCameraIndex(activeCamera);
         javaCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
         javaCameraView.setCvCameraViewListener(this);
-           javaCameraView.enableFpsMeter();
-   //     javaCameraView.setMaxFrameSize(size, size);
-//        javaCameraView.setMinimumHeight(size);
+        javaCameraView.enableFpsMeter();
 
     }
+
     @Override
     public void onCameraViewStarted(int width, int height) {
-
+        mRgba = new Mat();
         startListening();
     }
 
     @Override
     public void onCameraViewStopped() {
+        mRgba.release();
         thread.interrupt();
     }
 
+    Mat mRgba;
+
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-//        Log.i(TAG, "onCameraFrame: "+javaCameraView.mFpsMeter.getFPS()); // changed in oryginal opencv - changed camerabride to public fpsmeater, and FPSMeter added getFPS
-        Mat mRgba = inputFrame.rgba();
-       Mat tempMat = new Mat(mRgba.rows(),mRgba.cols(),mRgba.type());
+        mRgba = inputFrame.rgba();
 
-        switch (filterColorType){
-            case 0:
+//        Mat tempMat = new Mat(mRgba.rows(), mRgba.cols(), mRgba.type());
 
-                colorModifierMat(mRgba).copyTo(tempMat);
-                break;
-            case 1:
 
-                colorModifierMat(mRgba).copyTo(tempMat);
-                break;
-            case 2:
+        // po co color modifier podstawowy skoro mozna to z kanalow wyciagnac.
+//        switch (filterColorType){
+//            case 0:
+//
+//                colorModifierMat(mRgba).copyTo(tempMat);
+//                break;
+//            case 1:
+//
+//                colorModifierMat(mRgba).copyTo(tempMat);
+//                break;
+//            case 2:
+//
+//                colorModifierMat(mRgba).copyTo(tempMat);
+//                break;
+//            case 3:
+//
+//             grayScaleMod(mRgba).copyTo(tempMat);;
+//                break;
+//            default:
+//           grayScaleMod(mRgba).copyTo(tempMat);;
+//                break;
+//        }
 
-                colorModifierMat(mRgba).copyTo(tempMat);
-                break;
-            case 3:
 
-             grayScaleMod(mRgba).copyTo(tempMat);;
-                break;
-            default:
-           grayScaleMod(mRgba).copyTo(tempMat);;
-                break;
+        //mozna wszystko zawrzec w orginale
+//        switch (filterColorType) {
+//            case 0:
+//            case 2:
+//            case 1:
+//
+//                mRgba.copyTo(tempMat);
+//                break;
+//            case 3:
+//
+//                grayScaleMod(mRgba).copyTo(tempMat);
+//                break;
+//            default:
+//                grayScaleMod(mRgba).copyTo(tempMat);
+//                break;
+//        }
+
+
+        ArrayList<Mat> dst = new ArrayList<>(3);
+
+        if (filterColorType == 3) {
+            grayScaleMod(mRgba).copyTo(mRgba);
+        } else {
+            Core.split(mRgba, dst);
+
         }
 
 
+        if (graphOn) {
 
 
-
-
-
-        if (graphOn){
-            //to daje n czerowono na razie?
-
-
-            switch (filterColorType){
+            switch (filterColorType) {
                 case 0:
 
-                    pushValueToQueue(tempMat.get(tempMat.rows()/2, tempMat.cols()/2)[0]);
+//                    pushValueToQueue(mRgba.get(mRgba.rows() / 2, mRgba.cols() / 2)[0]);
+                    pushValueToQueue(dst.get(0).get(mRgba.rows() / 2, mRgba.cols() / 2)[0]);
+                    dst.set(1,Mat.zeros(mRgba.size(),dst.get(1).type()));
+                    dst.set(2,Mat.zeros(mRgba.size(),dst.get(2).type()));
+
+                    Core.merge(dst,mRgba);
 
                     break;
                 case 1:
 
-                    pushValueToQueue(tempMat.get(tempMat.rows()/2, tempMat.cols()/2)[1]);
+//                    pushValueToQueue(mRgba.get(mRgba.rows() / 2, mRgba.cols() / 2)[1]);
+                    pushValueToQueue(dst.get(1).get(mRgba.rows() / 2, mRgba.cols() / 2)[0]);
+                    dst.set(0,Mat.zeros(mRgba.size(),dst.get(0).type()));
+                    dst.set(2,Mat.zeros(mRgba.size(),dst.get(2).type()));
+
+                    Core.merge(dst,mRgba);
+
 
                     break;
                 case 2:
 
-                    pushValueToQueue(tempMat.get(tempMat.rows()/2, tempMat.cols()/2)[2]);
+//                    pushValueToQueue(mRgba.get(mRgba.rows() / 2, mRgba.cols() / 2)[2]);
+                    pushValueToQueue(dst.get(2).get(mRgba.rows() / 2, mRgba.cols() / 2)[0]);
+                    dst.set(0,Mat.zeros(mRgba.size(),dst.get(0).type()));
+                    dst.set(1,Mat.zeros(mRgba.size(),dst.get(1).type()));
+
+                    Core.merge(dst,mRgba);
 
                     break;
                 case 3:
+                    //Linear luminance. Can be change to any other grayscale coversion
+//                    pushValueToQueue(
+//                            (0.2126 * mRgba.get(mRgba.rows() / 2, mRgba.cols() / 2)[0]) +
+//                                    (0.7152 * mRgba.get(mRgba.rows() / 2, mRgba.cols() / 2)[1]) +
+//                                    (0.0722 * mRgba.get(mRgba.rows() / 2, mRgba.cols() / 2)[2])
+//
+//                    );
 
-                    pushValueToQueue(tempMat.get(tempMat.rows()/2, tempMat.cols()/2)[0]);
+                    pushValueToQueue(mRgba.get(mRgba.rows() / 2, mRgba.cols() / 2)[0]);
 
                     break;
                 default:
-                    pushValueToQueue(tempMat.get(tempMat.rows()/2, tempMat.cols()/2)[0]);
+//                    pushValueToQueue(
+//                            (0.2126 * mRgba.get(mRgba.rows() / 2, mRgba.cols() / 2)[0]) +
+//                                    (0.7152 * mRgba.get(mRgba.rows() / 2, mRgba.cols() / 2)[1]) +
+//                                    (0.0722 * mRgba.get(mRgba.rows() / 2, mRgba.cols() / 2)[2])
+//
+//                    );
+                    pushValueToQueue(mRgba.get(mRgba.rows() / 2, mRgba.cols() / 2)[0]);
 
                     break;
             }
 
 
-
-
-
         }
 
-//        dataMat.release();
 
 
-//        Rect rectCrop = new Rect(0,0,mRgba.cols(),mRgba.rows()/2);
-//        Mat image_output= mRgba.submat(rectCrop);
-//
-//        Imgproc.resize(image_output,image_output,mRgba.size());
-        mRgba.release();
+        if (cameraOn) {
 
-if (cameraOn){
-    return tempMat;
-}
-else{
-return null;
-}
+            switch (filterColorType) {
+                case 0:
+//                    return dst.get(0);
+                    return mRgba;
+
+                case 1:
+                                       return mRgba;
+//                    return dst.get(1);
+                case 2:
+                    return mRgba;
+//                    return dst.get(2);
+                default:
+                    return mRgba;
+
+            }
+
+        } else {
+            return null;
+        }
 
     }
 
     Queue<Double> valuesToAnalize = new LinkedList<Double>();
     Queue<Double> FPStoAnalize = new LinkedList<Double>();
 
-    public void pushValueToQueue(double value){
-        synchronized (monitor){
+    public void pushValueToQueue(double value) {
+        synchronized (monitor) {
 
             valuesToAnalize.add(value);
             FPStoAnalize.add(javaCameraView.mFpsMeter.getFPS());
@@ -287,7 +362,6 @@ return null;
 
     private final Object monitor = new Object();
     private Thread thread;
-
 
 
     public void startListening() {
@@ -307,8 +381,6 @@ return null;
     }
 
 
-    // zy jezeli szybciej pobierze i aanalizuje dane niz wyswietli to wywali?
-    //czy aktualizacja wykresu te nie powinna byc jako osobny thread?
     private void analizeThread() throws InterruptedException {
 
         while (!thread.isInterrupted()) {
@@ -330,10 +402,8 @@ return null;
                     fpsArray[i] = FPStoAnalize.poll();
                 }
 
-                Log.i(TAG, "analizeThread: " + average(fpsArray));
 
                 Phasor[] testDFT = singlalTransform(test, average(fpsArray)); // do pobrania fps;
-
 
 
                 double[] ampParcel = new double[testDFT.length / 2];
@@ -342,7 +412,7 @@ return null;
 
 
                 for (int i = 0; i < testDFT.length / 2; i++) {
-                    Log.i(TAG, "run: " + testDFT[i].toString2());
+                    //   Log.i(TAG, "run: " + testDFT[i].toString2());
                     // freqList[i]=testDFT[i].getAmp();
 
                     ampParcel[i] = testDFT[i].getAmp();
@@ -359,7 +429,7 @@ return null;
                 }
 
                 LineDataSet dataSet = new LineDataSet(entries, "Signal");
-               // dataSet.setLabel("Signal strength");
+                // dataSet.setLabel("Signal strength");
 
                 dataSet.setDrawValues(false);
                 LineData lineData = new LineData(dataSet);
@@ -367,7 +437,6 @@ return null;
 
                 XAxis xAxis = chart.getXAxis();
                 xAxis.setDrawLabels(true);
-
 
 
                 YAxis yAxis = chart.getAxisRight();
@@ -409,7 +478,7 @@ return null;
             javaCameraView.disableView();
 
         }
-        if (thread != null){
+        if (thread != null) {
             thread.interrupt();
         }
 
@@ -423,7 +492,7 @@ return null;
             javaCameraView.disableView();
 
         }
-        if (thread != null){
+        if (thread != null) {
             thread.interrupt();
         }
 
@@ -455,20 +524,19 @@ return null;
     }
 
 
-
     public double average(double[] data) {
         double sum = 0;
         double average;
 
-        for(int i=0; i < data.length; i++){
+        for (int i = 0; i < data.length; i++) {
             sum = sum + data[i];
         }
-        average = (double)sum/data.length;
+        average = (double) sum / data.length;
         return average;
     }
 
 
-    public Mat grayScaleMod(Mat inputMat){
+    public Mat grayScaleMod(Mat inputMat) {
 
         Mat tempMat = inputMat;
         Imgproc.cvtColor(inputMat, tempMat, Imgproc.COLOR_RGB2GRAY);
@@ -478,58 +546,56 @@ return null;
     }
 
 
-
-
-    int initialColor = 0xffffff;
-    int filterColor = initialColor;
-    public Mat colorModifierMat(Mat inputMat){
-        // Log.i(TAG, "colorModifierMat: " +inputMat.type());
-        double redPercent = Color.red(filterColor) / 255.0 ;
-        double greenPercent = Color.green(filterColor) / 255.0;
-        double bluePercent = Color.blue(filterColor) /255.0;
-
-//    Log.i(TAG, "colorModifierMat: "+inputMat.cols() + "cols" + inputMat.rows() + "rows");
-
-        inputMat.convertTo(inputMat, CvType.CV_64FC3);
-//    inputMat.convertTo(inputMat, CvType.CV_8UC3);
-        int size = (int) (inputMat.total() * inputMat.channels());
-        double[] temp = new double[size];
-//    Log.i(TAG, "colorModifierMat: size" + size);
-
-        inputMat.get(0, 0, temp);
-        for (int i = 0; i < size-4; i=i+4)
-        {
-
-            temp[i] =  (temp[i]*redPercent);
-
-            temp[i+1] =  (temp[i+1]*greenPercent);
-            temp[i+2] =  (temp[i+2] *bluePercent);
-            temp[i+3] =  255;
-        }
-
-        inputMat.put(0, 0, temp);
-        inputMat.convertTo(inputMat, CvType.CV_8UC4);
-        return inputMat;
-    }
+//    int initialColor = 0xffffff;
+//    int filterColor = initialColor;
+//    public Mat colorModifierMat(Mat inputMat){
+//        // Log.i(TAG, "colorModifierMat: " +inputMat.type());
+//        double redPercent = Color.red(filterColor) / 255.0 ;
+//        double greenPercent = Color.green(filterColor) / 255.0;
+//        double bluePercent = Color.blue(filterColor) /255.0;
+//
+////    Log.i(TAG, "colorModifierMat: "+inputMat.cols() + "cols" + inputMat.rows() + "rows");
+//
+//        inputMat.convertTo(inputMat, CvType.CV_64FC3);
+////    inputMat.convertTo(inputMat, CvType.CV_8UC3);
+//        int size = (int) (inputMat.total() * inputMat.channels());
+//        double[] temp = new double[size];
+////    Log.i(TAG, "colorModifierMat: size" + size);
+//
+//        inputMat.get(0, 0, temp);
+//        for (int i = 0; i < size-4; i=i+4)
+//        {
+//
+//            temp[i] =  (temp[i]*redPercent);
+//
+//            temp[i+1] =  (temp[i+1]*greenPercent);
+//            temp[i+2] =  (temp[i+2] *bluePercent);
+//            temp[i+3] =  255;
+//        }
+//
+//        inputMat.put(0, 0, temp);
+//        inputMat.convertTo(inputMat, CvType.CV_8UC4);
+//        return inputMat;
+//    }
 
     @Override
     public boolean onMenuItemClick(MenuItem item) {
         int id = item.getItemId();
-        switch (id){
+        switch (id) {
             case R.id.red:
                 filterColorType = 0;
-                filterColor = 0xff0000;
+//                filterColor = 0xff0000;
                 return true;
             case R.id.green:
                 filterColorType = 1;
-                filterColor = 0x00ff00;
-                return true;
+//                filterColor = 0x00ff00;
+               return true;
             case R.id.blue:
                 filterColorType = 2;
-                filterColor = 0x0000ff;
+//                filterColor = 0x0000ff;
                 return true;
             case R.id.gray:
-                filterColorType =3;
+                filterColorType = 3;
 
                 return true;
             default:
@@ -537,8 +603,20 @@ return null;
         }
     }
 
-    public void flashOnOff(View view )throws CameraAccessException {
+    public void flashOnOff(View view) throws CameraAccessException {
 
         javaCameraView.toggleFlashMode();
     }
+
+
+    public int getIntFromColor(int R, int G, int B) {
+
+        R = (R << 16) & 0x00FF0000;
+        G = (G << 8) & 0x0000FF00;
+        B = B & 0x000000FF;
+
+        return 0xFF000000 | R | G | B;
     }
+
+
+}
